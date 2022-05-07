fee2039c6a1427ebaefcb94b972b74a07a197da9
Timothee Denizou <timothee.denizou@epita.fr>
2022-05-07 16:58:18 +0200

    testing

--- benches/benches.rs
+++ benches/benches.rs
@@ -5,7 +5,7 @@ extern crate wasm_game_of_life;
 
 #[bench]
 fn universe_ticks(b: &mut test::Bencher) {
-    let mut universe = wasm_game_of_life::Universe::new();
+    let mut universe = wasm_game_of_life::universe::Universe::new();
 
     b.iter(|| {
         universe.tick();
